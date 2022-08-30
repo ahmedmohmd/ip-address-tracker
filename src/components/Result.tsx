b4fc7b873605b11109d1fd4115c91b57dddcc2e8
Ahmed Mohamed <er.ahmed.3301@gmail.com>
2022-08-30 16:38:13 +0200

    initial commit

---
+++ src/components/Result.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Result: FC = () => {
+  return (
+    <article>
+      <h1>Result Component</h1>
+    </article>
+  );
+};
+
+export default Result;
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const SearchBar: FC = () => {
+  return (
+    <article>
+      <h1>SearchBar Component</h1>
+    </article>
+  );
+};
+
+export default SearchBar;
