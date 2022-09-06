3f59d74a9dd95a0a53a73de678548c91b978788d
Ahmed Mohamed <er.ahmed.3301@gmail.com>
2022-09-06 10:16:39 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -77,10 +77,7 @@ const App: FC = () => {
       <figure className={clsx("w-full h-auto", "grow-[2] basis-2")}>
         <img
           className={clsx("w-full h-full", "object-cover")}
-          src={`https://maps.googleapis.com/maps/api/staticmap?center=Brooklyn+Bridge,New+York,NY&zoom=13&size=600x300&maptype=roadmap
-          &markers=color:blue%7Clabel:S%7C40.702147,-74.015794&markers=color:green%7Clabel:G%7C40.711614,-74.012318
-          &markers=color:red%7Clabel:C%7C40.718217,-73.998284
-          &key=AIzaSyDOyLfwxUo4TeokgkFAq6OOLkQR7L2Pu9w&signature=YOUR_SIGNATURE`}
+          src={`https://maps.googleapis.com/maps/api/staticmap?center=40.714%2c%20-73.998&zoom=12&size=400x400&key=AIzaSyAW5WNn6x8Pc_J6BVEahd59i8MFbQnDuJ4`}
           alt="Map"
         />
       </figure>
